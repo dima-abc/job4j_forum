5b74ea74ed31dc75bae15d99aafa78eaf6e0d624
dmitryjob4j <haoos@inbox.ru>
2022-07-08 16:02:23 +0300

    0. Spring test [#6881]
    add codecov branch

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 [![Java CI with Maven Job4j_Forum](https://github.com/Dima-Stepanov/job4j_forum/actions/workflows/maven.yml/badge.svg)](https://github.com/Dima-Stepanov/job4j_forum/actions/workflows/maven.yml)
+[![codecov](https://codecov.io/gh/Dima-Stepanov/job4j_forum/branch/master/graph/badge.svg?token=0uMjbFhS0N)](https://codecov.io/gh/Dima-Stepanov/job4j_forum)
 
 <h1>Проект Job4j_Forum</h1>
 <h3>Стек Spring Boot, thymeleaf, bootstrap, PostgreSQL</h3>
---
+++ src/test/java/ru/job4j/forum/controller/PostControlTest.java
@@ -0,0 +1,95 @@
+package ru.job4j.forum.controller;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.job4j.forum.ForumApplication;
+import ru.job4j.forum.model.Post;
+import ru.job4j.forum.service.PostService;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+/**
+ * 3. Мидл
+ * 3.4. Spring
+ * 3.4.5. Boot
+ * 0. Spring test [#6881]
+ * PostControl TEST GET методы.
+ *
+ * @author Dmitry Stepanov, user Dmitry
+ * @since 08.07.2022
+ */
+@SpringBootTest(classes = ForumApplication.class)
+@AutoConfigureMockMvc
+class PostControlTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private PostService postService;
+
+    @Test
+    @WithMockUser
+    void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/index"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("post/index"));
+    }
+
+    @Test
+    @WithMockUser
+    void postGetOk() throws Exception {
+        Post post = postService.savePost(new Post());
+        this.mockMvc.perform(get("/post/" + post.getId()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("post/post"));
+    }
+
+    @Test
+    @WithMockUser
+    void postGetFail() throws Exception {
+        this.mockMvc.perform(get("/post/-1"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
+    }
+
+    @Test
+    @WithMockUser
+    void newPost() throws Exception {
+        this.mockMvc.perform(get("/newPost"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("post/edit"));
+    }
+
+    @Test
+    @WithMockUser
+    void editPostOk() throws Exception {
+        Post post = postService.savePost(new Post());
+        this.mockMvc.perform(get("/editPost/" + post.getId()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("post/edit"));
+    }
+
+    @Test
+    @WithMockUser
+    void editPostFail() throws Exception {
+        this.mockMvc.perform(get("/editPost/-1"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
+    }
+}
