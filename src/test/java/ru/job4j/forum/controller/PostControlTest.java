1bde30c5ed86c7a18506dd13f00bf905ed0e27e7
dmitryjob4j <haoos@inbox.ru>
2022-07-08 16:02:23 +0300

    0. Spring test [#6881]

---
+++ src/test/java/ru/job4j/forum/controller/PostControlTest.java
@@ -0,0 +1,85 @@
+package ru.job4j.forum.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.job4j.forum.ForumApplication;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+/**
+ * 3. Мидл
+ * 3.4. Spring
+ * 3.4.5. Boot
+ * 0. Spring test [#6881]
+ * PostControl TEST GET методы.
+ *
+ * @author Dmitry Stepanov, user Dmitry
+ * @since 08.07.2022
+ */
+@SpringBootTest(classes = ForumApplication.class)
+@AutoConfigureMockMvc
+class PostControlTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/index"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("post/index"));
+    }
+
+    @Test
+    @WithMockUser
+    void postGetOk() throws Exception {
+        this.mockMvc.perform(get("/post/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("post/post"));
+    }
+
+    @Test
+    @WithMockUser
+    void postGetFail() throws Exception {
+        this.mockMvc.perform(get("/post/-1"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
+    }
+
+    @Test
+    @WithMockUser
+    void newPost() throws Exception {
+        this.mockMvc.perform(get("/newPost"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("post/edit"));
+    }
+
+    @Test
+    @WithMockUser
+    void editPostOk() throws Exception {
+        this.mockMvc.perform(get("/editPost/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("post/edit"));
+    }
+
+    @Test
+    @WithMockUser
+    void editPostFail() throws Exception {
+        this.mockMvc.perform(get("/editPost/-1"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
+    }
+}
---
+++ src/test/java/ru/job4j/forum/controller/RegControlTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.forum.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.job4j.forum.ForumApplication;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+/**
+ * 3. Мидл
+ * 3.4. Spring
+ * 3.4.5. Boot
+ * 0. Spring test [#6881]
+ * RegControl TEST GET методы
+ *
+ * @author Dmitry Stepanov, user Dmitry
+ * @since 08.07.2022
+ */
+@SpringBootTest(classes = ForumApplication.class)
+@AutoConfigureMockMvc
+class RegControlTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void regPage() throws Exception {
+        this.mockMvc.perform(get("/reg"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("user/reg"));
+    }
+}
